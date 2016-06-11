e53a11114f5e5e9bcc840fdd3120753dc4a35b0c
Jeremy Clarke <geckojsc@gmail.com>
2016-06-11 23:04:31 +0100

    fixed debugworlddraw for love 0.10.0+

--- source/debugworlddraw.lua
+++ source/debugworlddraw.lua
@@ -79,10 +79,10 @@ local function debugWorldDraw(world,topLeft_x,topLeft_y,width,height)
 			love.graphics.line(x1,y1,x2,y2)
 		else
 			if (x1) then
-				love.graphics.point(x1,y1)
+				love.graphics.points(x1,y1)
 			end
 			if (x2) then
-				love.graphics.point(x2,y2)
+				love.graphics.points(x2,y2)
 			end
 		end
 	end
@@ -93,10 +93,10 @@ local function debugWorldDraw(world,topLeft_x,topLeft_y,width,height)
 	for i=1,#contacts do
 		local x1,y1,x2,y2 = contacts[i]:getPositions()
 		if (x1) then
-			love.graphics.point(x1,y1)
+			love.graphics.points(x1,y1)
 		end
 		if (x2) then
-			love.graphics.point(x2,y2)
+			love.graphics.points(x2,y2)
 		end
 	end
 end
