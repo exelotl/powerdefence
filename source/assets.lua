e8d945ebfebe8a53c590b7c301ca7f32c4bfaf78
Jeremy Clarke <geckojsc@gmail.com>
2016-06-11 21:48:01 +0100

    slightly bigger player sprites, centred player origin

--- source/assets.lua
+++ source/assets.lua
@@ -1,5 +1,16 @@
 local assets = {}
 
+local function makeQuads(img, quadw, quadh)
+	local w, h = img:getDimensions()
+	local t = {}
+	for x=0, w-1, quadw do
+		for y=0, h-1, quadh do
+			table.insert(t, lg.newQuad(x, y, quadw, quadh, w, h))
+		end
+	end
+	return t
+end
+
 function assets.load()
 	
 	lg.setDefaultFilter("nearest", "nearest") -- for sharp pixel zooming
@@ -10,12 +21,7 @@ function assets.load()
 		lg.newImage("assets/player_pink.png"),
 		lg.newImage("assets/player_yellow.png")
 	}
-	
-	-- animation frames for all player graphics
-	assets.playerq = {
-		lg.newQuad(0, 0, 16, 16, assets.player[1]:getDimensions()),
-		lg.newQuad(16, 0, 16, 16, assets.player[1]:getDimensions()),
-	}
+	assets.playerq = makeQuads(assets.player[1], 16, 16)
 	
 	assets.background = love.graphics.newImage("assets/placeholders/floor.png")
 	assets.fft = love.graphics.newImage("assets/placeholders/forcefieldtop.png")
