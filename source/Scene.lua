3b610b967b8555f977e7fa3e8773c83818f9f5a8
Jeremy Clarke <geckojsc@gmail.com>
2016-06-12 20:29:10 +0100

    removing an object from the scene also removes from physics world

--- source/Scene.lua
+++ source/Scene.lua
@@ -10,13 +10,19 @@ end
 
 function Scene:update(dt)
 	for _,e in ipairs(self.removelist) do
-		if e.added then e:removed() end
+		-- activate removal callback
+		if e.removed then e:removed() end
 		e.scene = nil
+		-- remove from the entity list
 		for i, e2 in ipairs(self.entities) do
 			if e == e2 then
 				table.remove(self.entities, i)
 				break
 			end
+		end
+		-- remove from the physics world
+		if e.body then
+			e.body:destroy()
 		end
 	end
 	for _,e in ipairs(self.addlist) do
