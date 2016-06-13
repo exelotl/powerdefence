c54f5ebcb7cf823846db533f2ad4e4d7955540ad
Ben <benjohnson_321@msn.com>
2016-06-13 10:08:31 +0100

    EXPLOSIONS!!!
    explosion graphic and explosion screenshake

--- source/Explosion.lua
+++ source/Explosion.lua
@@ -1,7 +1,11 @@
 local Explosion = oo.class()
+local Anim = require "Anim"
+
+local ANIM_EXPLOSION = {1,2,3,4,5,6,7,8,9,10,11, rate = 15, loop = false}
 
 function Explosion:init(scene, x, y, radius, duration)
-	self.duration = duration
+	self.anim = Anim.new(ANIM_EXPLOSION)
+    self.duration = duration
     self.initx = x
     self.inity = y
     self.type = "orb"
@@ -16,19 +20,21 @@ function Explosion:added()
     self.fixture = lp.newFixture(self.body, self.shape)
     self.fixture:setSensor(true)
 	self.fixture:setUserData({dataType='explosion', data=self})
+    self.anim:play(ANIM_EXPLOSION)
 end
 
 function Explosion:update(dt)
     self.duration = self.duration - dt
+    screenShake = screenShake + 1
     if self.duration < 0 then
         self.scene:remove(self)
     end
-    
+    self.anim:update(dt)
 end
 
 function Explosion:draw()
     local x,y = self.body:getPosition()
-    lg.circle("fill",x,y,self.radius)
+    lg.draw(assets.explosion, assets.explosionq[self.anim.frame], x, y,0,1,1,32,32)
 end
 
 function Explosion:takeDamage()
