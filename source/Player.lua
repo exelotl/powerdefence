666395c42bc29d781598dad272cd0af458c57668
mbway <matthewbroadway@msn.com>
2016-06-13 09:10:54 +0100

    added more death state logic

--- source/Player.lua
+++ source/Player.lua
@@ -145,6 +145,7 @@ function Player:takeDamage()
     self.hp = self.hp - 1
     if not self:isAlive() then
 		self.fixture:setMask(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)
+		self.type = 'deadPlayer'
     end
 end
 
