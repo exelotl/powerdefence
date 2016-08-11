51749850fe280a094cbf4e4e140c414edaa2995b
mbway <matthewbroadway@msn.com>
2016-08-11 13:17:50 +0100

    improved construction of list of droppable weapons

--- source/WeaponPickup.lua
+++ source/WeaponPickup.lua
@@ -2,21 +2,25 @@ local weapons = require "weapons"
 
 local WP = oo.class()
 
+
+local excludedWeapons = {None=true}
+local droppableWeapons = {}
+
+for name, _ in pairs(weapons) do
+    if not excludedWeapons[name] then
+        table.insert(droppableWeapons, name)
+    end
+end
+
+
 function WP:init(scene, x, y)
     self.type = "pickup"
     scene:add(self)
     self.body = lp.newBody(scene.world, x, y, 'dynamic')
 
     --pick a random weapon
-    local keyset = {}
-    for k, v in pairs(weapons) do
-        if not v.excludeFromPickups then
-            table.insert(keyset, k)
-        end
-    end
-
     -- random gives integer from 1 to n
-    self.weapon = weapons[keyset[math.random(#keyset)]]
+    self.weapon = weapons[droppableWeapons[math.random(#droppableWeapons)]]
 
     local image = assets.weaponso[self.weapon.image]
 
